8dc7b41b6cdf9005156bcc4d698d736a46cca3d2
yash <yahsveersingh@innotical.com>
2017-09-26 09:33:39 +0530

    Update

---
+++ src/app/home/nested/nested.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nested-root',
+  templateUrl: './nested.component.html',
+  //styleUrls:  ['./footer.component.css']
+})
+export class nestedComponent {
+  title = ``;
+}
+
+
